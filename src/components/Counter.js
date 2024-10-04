bd94052f2e0f03f03f7bde5ddd9484c621757c4a
unknown <amincxo@gmail.com>
2024-10-05 01:11:11 +0330

    use dispatch hook for dispatch acction

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector , useDispatch } from 'react-redux'
 import { selectcounter } from '../features/counter/counterSlice'
+import { increment , decrement } from '../features/counter/counterSlice';
 
 export default function Counter() {
-    const conter = useSelector(selectcounter)
+    const conter = useSelector(selectcounter);
+    const dispatch = useDispatch();
     return (
     <div>
         <h2>counter is {conter}</h2>
+        <button onClick={()=> dispatch(increment())} >increment</button>
+        <button onClick={()=> dispatch(decrement())} >decrement</button>
     </div>
   )
 }
