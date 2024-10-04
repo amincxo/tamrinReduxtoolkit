c756889515b46a8c59008d8ba35f723c6421940c
unknown <amincxo@gmail.com>
2024-10-04 23:01:45 +0330

    create store

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import counterReducer from "../features/counter/counterSlice"
+
+const store = configureStore({
+    reducer: {
+        counter:counterReducer
+    },
+})
