45018bf40261f5487756984237db3f713c38bdbe
unknown <amincxo@gmail.com>
2024-10-07 14:21:50 +0330

    change store for  users reducers

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from "../features/counter/counterSlice"
 import numberReducer from "../features/number/numberSlice"
+import usersReducers from "../features/users/usersSlice"
 
 import logger from "redux-logger"
 
 const store = configureStore({
     reducer: {
         counter:counterReducer,
-        number:numberReducer
+        number:numberReducer,
+        users: usersReducers
     },
     middleware : (getDefaultMiddleware => getDefaultMiddleware().concat(logger))
 })
